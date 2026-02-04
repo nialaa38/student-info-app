22ffff8f43ea3c65714a5188b10091fd6a502f4e
nialaa38 <your-nialaa38-nialaaila38@gmail..com>
2026-02-04 15:59:22 +0800

    second commit

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,8 +2,21 @@ import '@testing-library/jest-dom';
 import 'whatwg-fetch';
 
 // Polyfills for jsdom environment
-if (typeof global !== 'undefined' && typeof global.TextEncoder === 'undefined') {
-  const { TextEncoder, TextDecoder } = require('util');
-  global.TextEncoder = TextEncoder;
-  global.TextDecoder = TextDecoder;
+if (typeof globalThis !== 'undefined' && typeof globalThis.TextEncoder === 'undefined') {
+  // Simple polyfill for TextEncoder/TextDecoder in test environment
+  (globalThis as any).TextEncoder = class TextEncoder {
+    encode(input: string = ''): Uint8Array {
+      const encoder = new Array(input.length);
+      for (let i = 0; i < input.length; i++) {
+        encoder[i] = input.charCodeAt(i);
+      }
+      return new Uint8Array(encoder);
+    }
+  };
+  
+  (globalThis as any).TextDecoder = class TextDecoder {
+    decode(input: Uint8Array): string {
+      return String.fromCharCode(...Array.from(input));
+    }
+  };
 }
